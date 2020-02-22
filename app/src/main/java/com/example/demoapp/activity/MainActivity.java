61169bc731817d6aafdfccfbcfb1c32d22b9be48
ruslan <Rusl.215k58@gmail.com>
2020-02-22 23:20:25 +0400

    Replaced bottom navigation menu

--- app/src/main/java/com/example/demoapp/activity/MainActivity.java
+++ app/src/main/java/com/example/demoapp/activity/MainActivity.java
@@ -2,24 +2,18 @@ package com.example.demoapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
-import android.nfc.NfcAdapter;
-import android.os.Bundle;
-import android.view.View;
-
-import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.example.demoapp.R;
 import com.example.demoapp.api.ShowFragmentListener;
 import com.example.demoapp.api.chat.Message;
 import com.example.demoapp.fragments.FragmentTime;
 import com.example.demoapp.fragments.MailFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -27,16 +21,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
-
 public class MainActivity extends AppCompatActivity implements ShowFragmentListener, MailFragment.OnButtonClickListener, MailFragment.OnAudioUploaded {
-
-    private static final int mail = 1;
-    private static final int status = 2;
-    private static final int time = 3;
-    private static final int nfc = 4;
-    private static final int profile = 5;
 
     private DatabaseReference reference;
     private String userID;
@@ -56,44 +41,29 @@ public class MainActivity extends AppCompatActivity implements ShowFragmentListe
     @Override
     protected void onStart() {
         super.onStart();
-        MeowBottomNavigation meowBottomNavigation = findViewById(R.id.bottomNav);
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(mail, R.drawable.ic_menu));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(status, R.drawable.ic_menu));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(time, R.drawable.ic_menu));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(nfc, R.drawable.ic_menu));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(profile, R.drawable.ic_menu));
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setSelectedItemId(R.id.nav_time);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_nfc:
+                    switchTo(new MailFragment());
+                    break;
+                case R.id.nav_chat:
+                    switchTo(new MailFragment());
+                    break;
+                case R.id.nav_time:
+                    switchTo(new FragmentTime());
+                    break;
+                case R.id.nav_search:
+                    switchTo(new MailFragment());
+                    break;
+                case R.id.nav_profile:
+                    switchTo(new MailFragment());
+                    break;
 
-        meowBottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
-            @Override
-            public Unit invoke(MeowBottomNavigation.Model model) {
-                switch (model.getId()) {
-                    case mail:
-
-                        switchTo(new MailFragment());
-                        break;
-                    case status:
-
-                        switchTo(new MailFragment());
-                        break;
-                    case time:
-
-                        switchTo(new FragmentTime());
-                        break;
-                    case nfc:
-
-                        switchTo(new MailFragment());
-                        break;
-
-                    case profile:
-                        switchTo(new MailFragment());
-                        break;
-                }
-                return null;
             }
+            return true;
         });
-
-        //Страница по умолчианию
-        meowBottomNavigation.show(time, true);
         switchTo(new FragmentTime());
     }
 
